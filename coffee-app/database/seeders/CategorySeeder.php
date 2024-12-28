1e63470a60c43c3553a6c938ee3829307312ce59
VOIDkuugeki <voidkuugeki@gmail.com>
2024-12-28 07:57:15 +0700

    finish section 2

--- coffee-app/database/seeders/CategorySeeder.php
+++ coffee-app/database/seeders/CategorySeeder.php
@@ -4,34 +4,23 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class CategorySeeder extends Seeder
 {
-    public $categories = [
-        [
-            'id' => 1,
-            'name' => 'Cafe',
-            'description' => '',
-        ],
-        [
-            'id' => 2,
-            'name' => 'Chicken',
-            'description' => '',
-        ],
-    ];
-
-    private function getCategories()
-    {
-        return $this->categories;
-    }
-
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        foreach ($this->getCategories() as $category) {
-            Category::create($category);
+        $faker = Faker::create();
+
+        // Tạo 1000 records
+        for ($i = 1; $i <= 1000; $i++) {
+            Category::create([
+                'name' => $faker->word, // Tên ngẫu nhiên
+                'description' => $faker->sentence, // Mô tả ngẫu nhiên
+            ]);
         }
     }
 }
